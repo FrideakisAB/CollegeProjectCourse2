581a9dc4924f57a30dadf1a54aeed0e38d6460ea
FrideakisAB <reterdastion@gmail.com>
2021-05-20 01:57:53 +0300

    Создано главное меню Создано меню создание и просмотра персонажей Создана таблица персонажей Реализовано обращение к БД, примитивное создание пользователя и проверка доступности имени

--- CollegeProjectCourse2/MainWindow.xaml.cs
+++ CollegeProjectCourse2/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace CollegeProjectCourse2
 {
@@ -20,9 +21,64 @@ namespace CollegeProjectCourse2
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection connection;
+
         public MainWindow()
         {
+            string dbFilePath = System.IO.Directory.GetCurrentDirectory() + "\\Database.mdf";
+            connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + dbFilePath + ";Integrated Security=True");
+            connection.Open();
+
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            connection.Close();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            createUserMenu.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            createUserMenu.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            viewerUserMenu.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            viewerUserMenu.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            // Need check all values, and return if data broken
+            SqlCommand command = new SqlCommand(
+                "INSERT INTO Players (name, history, legend, forces, family, gender, fireball, vampire, image)" +
+                $"VALUES (N'{createName.Text}', N'{createHistory.Text}', N'{createLegend.Text}', N'{createForces.Text}', N'{createFamily.Text}', " +
+                $"{(((bool)createGender.IsChecked) ? 1 : 0)}, {(((bool)createFire.IsChecked) ? 1 : 0)}, {(((bool)createVamp.IsChecked) ? 1 : 0)}, 0)", connection); // Set image!!!
+            command.ExecuteNonQuery();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            connection.Close();
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            SqlCommand command = new SqlCommand($"SELECT COUNT(name) FROM Players WHERE name = N'{createName.Text}'", connection);
+            int cnt = (Int32)command.ExecuteScalar();
+            if (cnt == 0)
+                createName.Text = "True"; // Test, on production display message
         }
     }
 }
---
+++ CollegeProjectCourse2/dbo.Players.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Players]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [name] NVARCHAR(80) NOT NULL, 
+    [history] NVARCHAR(80) NOT NULL, 
+    [legend] NVARCHAR(300) NOT NULL, 
+    [forces] NVARCHAR(35) NOT NULL, 
+    [family] NVARCHAR(35) NOT NULL, 
+    [gender] BIT NOT NULL, 
+    [fireball] BIT NOT NULL, 
+    [vampire] BIT NOT NULL, 
+    [image] INT NOT NULL
+)
